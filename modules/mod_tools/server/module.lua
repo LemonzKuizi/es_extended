dac1da868c175c31c4c64f69795599e1ea334a82
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-19 22:05:17 -0300

    fix: add dump var

--- modules/mod_tools/server/module.lua
+++ modules/mod_tools/server/module.lua
@@ -1,7 +1,8 @@
+msg = ""
 RegisterCommand('alert', function(source, args, user)	
-    for i,v in pairs(args) do
-      msg = msg .. " " .. v
-    end
-    TriggerClientEvent("alert", -1, msg)
-    msg = ""    
+			for i,v in pairs(args) do
+				msg = msg .. " " .. v
+			end
+			TriggerClientEvent("alert", -1, msg)
+			msg = ""    
 end)
