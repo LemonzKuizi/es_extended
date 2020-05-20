6d6d53a9825f7e461c2a75140aec5a7d94d8984c
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-19 22:27:55 -0300

    accept null value

--- modules/mod_tools/server/module.lua
+++ modules/mod_tools/server/module.lua
@@ -1,6 +1,6 @@
 RegisterCommand('alert', function(source, args, user)
     for i,v in pairs(args) do
-      msg = msg .. " " .. v
+      msg = (msg or '') .. " " .. v
     end
     TriggerClientEvent("'mod_tools:alert", -1, msg)
     msg = ""
