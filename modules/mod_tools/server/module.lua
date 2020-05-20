1bb8c53b0893ddc11156db5ca79611e0ce1aba57
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-19 23:50:48 -0300

    another fix

--- modules/mod_tools/server/module.lua
+++ modules/mod_tools/server/module.lua
@@ -2,6 +2,6 @@ RegisterCommand('alert', function(source, args, user)
     for i,v in pairs(args) do
       msg = (msg or '') .. " " .. v
     end
-    TriggerClientEvent('alert', -1, msg)    
+    TriggerClientEvent('esx_mod_tools:alert', -1, msg)    
     print('Shut');
 end)
