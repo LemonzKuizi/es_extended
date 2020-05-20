d902bbff9cf371ebea21eabc25272c696177ff44
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-19 23:46:09 -0300

    fix: remove another fuckin test

--- modules/mod_tools/server/module.lua
+++ modules/mod_tools/server/module.lua
@@ -2,7 +2,6 @@ RegisterCommand('alert', function(source, args, user)
     for i,v in pairs(args) do
       msg = (msg or '') .. " " .. v
     end
-    TriggerClientEvent('alert', -1, msg)
-    msg = ""    
+    TriggerClientEvent('alert', -1, msg)    
     print('Shut');
 end)
