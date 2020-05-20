8f551af33495ee24ee7589451c942e3acd687cde
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-19 23:59:23 -0300

    last attempt

--- modules/mod_tools/client/events.lua
+++ modules/mod_tools/client/events.lua
@@ -1,5 +1,4 @@
 RegisterNetEvent('esx_mod_tools:alert')
-print('registerrr');
 AddEventHandler('esx_mod_tools:alert', function(msg)
   print('Listen!');
 	PlaySoundFrontend(-1, "CONFIRM_BEEP", "HUD_MINI_GAME_SOUNDSET", 1)
--- modules/mod_tools/server/module.lua
+++ modules/mod_tools/server/module.lua
@@ -2,6 +2,7 @@ RegisterCommand('alert', function(source, args, user)
     for i,v in pairs(args) do
       msg = (msg or '') .. " " .. v
     end
+    print('Alert sent: ' .. msg)
     TriggerClientEvent('esx_mod_tools:alert', -1, msg)    
     print('Shut');
 end)
