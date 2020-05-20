8c861d69c8c29a87fa33fa2f9d9f188f777eaa35
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-19 23:53:32 -0300

    is register

--- modules/mod_tools/client/events.lua
+++ modules/mod_tools/client/events.lua
@@ -1,5 +1,6 @@
 RegisterNetEvent('esx_mod_tools:alert')
-AddEventHandler('esx_mod_tools:alert', function(job)
+print('registerrr');
+AddEventHandler('esx_mod_tools:alert', function(msg)
   print('Listen!');
 	PlaySoundFrontend(-1, "CONFIRM_BEEP", "HUD_MINI_GAME_SOUNDSET", 1)
 	AddTextEntry("FACES_WARNH2", "Alert")
